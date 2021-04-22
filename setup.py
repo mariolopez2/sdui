427c46a00ff8b2cb2eaf81ff716e730f6a2ba977
Mario Lopez <mario.lopez@fii-na.com>
2021-04-22 18:49:30 -0500

    Version 1.2

--- setup.py
+++ setup.py
@@ -89,7 +89,8 @@ def iniciar_configuracion():
     configurar_dispositivo()
 
 def configurar_archivos():
-    os.system("sudo mv -f /home/pi/sdui/html /var/www/html/")
+    os.system("sudo rm -rf /var/www/")
+    os.system("sudo mv -f /home/pi/sdui/html /var/www/")
     print("Hemos terminado, el ultimo paso es necesario sea ejecutado de manera Manual. Por favor ejecute 'rclone config' y siga las instrucciones en el documento.")
 
 
